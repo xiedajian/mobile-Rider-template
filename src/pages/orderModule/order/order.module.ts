4cba0752c2d69d2ce613bc47f6687a00c2d245cc
谢大见 <172265972@qq.com>
2017-10-17 09:58:44 +0800

    2017.10.17

--- src/pages/orderModule/order/order.module.ts
+++ src/pages/orderModule/order/order.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { OrderPage } from './order';
-
+import { PipesModule } from '../../../pipes/pipes.module';
 @NgModule({
   declarations: [
     OrderPage,
   ],
   imports: [
+      PipesModule,
     IonicPageModule.forChild(OrderPage),
   ],
 })
